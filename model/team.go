256f5459c8d0e37441520343c9956a7912649619
omerfruk <omer.fruk3547@gmail.com>
2021-10-23 12:12:18 +0300

    Create : Team service & Model added

---
+++ model/team.go
@@ -0,0 +1,9 @@
+package model
+
+import "gorm.io/gorm"
+
+type Team struct {
+	gorm.Model
+	TeamName string   `json:"team_name"`
+	Players  []Player `json:"players"`
+}
---
+++ service/team.go
@@ -0,0 +1,35 @@
+package service
+
+import (
+	"github.com/nba-simulation/model"
+	"gorm.io/gorm"
+)
+
+type TeamService struct {
+	db *gorm.DB
+}
+
+func NewTeamService(db *gorm.DB) *TeamService {
+	return &TeamService{db: db}
+}
+
+func (s *TeamService) Create(m *model.Team) error {
+	err := s.db.Create(m).Error
+	return err
+}
+
+func (s *TeamService) GetById(id int64) (*model.Team, error) {
+	m := new(model.Team)
+	err := s.db.Where("id = ?", id).First(&m).Error
+	return m, err
+}
+
+func (s *TeamService) Update(m *model.Team) error {
+	err := s.db.Save(m).Error
+	return err
+}
+
+func (s *TeamService) Delete(m *model.Team) error {
+	err := s.db.Delete(m).Error
+	return err
+}
