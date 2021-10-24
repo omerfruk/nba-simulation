5bd8512f5d0b59fc6d6b17e016c6c37997671889
omerfruk <omer.fruk3547@gmail.com>
2021-10-24 21:25:30 +0300

    create : yeni vm eklendi donus tipi icin

--- viewmodels/scoreVM.go
+++ viewmodels/scoreVM.go
@@ -4,8 +4,23 @@ import "github.com/nba-simulation/model"
 
 type ScoreVM struct {
 	ScoreType model.ScoreType
-	TeamId    int
+	TeamId    uint
 	Team      model.Team
 	ScorerId  int
 	Scorer    PlayerVM
+}
+
+type ReturnVM struct {
+	MatchName   string
+	TeamScoreVM []TeamScoreVM
+}
+
+type TeamScoreVM struct {
+	TeamName      string
+	PlayerScoreVM []PlayerScoreVM
+}
+type PlayerScoreVM struct {
+	PlayerName      string
+	PlayerType      model.PlayerType
+	PlayerScoreType model.ScoreType
 }
